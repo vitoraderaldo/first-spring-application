ccb8959d11fbdfdfefa5514aa518eea133a8e48a
Vitor <vitor_aderaldo@trimble.com>
2020-07-25 13:02:21 -0300

    CRUD

---
+++ src/main/java/com/pluralsight/conferencedemo/controllers/HomeController.java
@@ -0,0 +1,25 @@
+package com.pluralsight.conferencedemo.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @Value("${app.version}")
+    private String appVersion;
+
+    @GetMapping
+    public Map getStatus(){
+        Map map = new HashMap<String, String>();
+        map.put("appVersion", appVersion);
+        return map;
+    }
+
+}
---
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SpeakerController.java
@@ -0,0 +1,46 @@
+package com.pluralsight.conferencedemo.controllers;
+
+import com.pluralsight.conferencedemo.models.Speaker;
+import com.pluralsight.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/speakers")
+public class SpeakerController {
+
+    @Autowired
+    SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> list(){
+        return speakerRepository.findAll();
+    }
+
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id){
+        return speakerRepository.getOne(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Speaker create(@RequestBody final Speaker speaker){
+        return speakerRepository.saveAndFlush(speaker);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id){
+        speakerRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker){
+        Speaker originalSpeaker = speakerRepository.getOne(id);
+        BeanUtils.copyProperties(speaker, originalSpeaker, "speaker_id");
+        return speakerRepository.saveAndFlush(originalSpeaker);
+    }
+}
